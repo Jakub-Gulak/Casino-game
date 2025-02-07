34937da5cd54776df09d8953ced02c1c6db3ec7b
Jakub-Gulak <jakubgulak@icloud.com>
2025-02-07 11:09:15 +0100

    (dpg.py) - add images files and add first one on page

--- gui/dpg.py
+++ gui/dpg.py
@@ -4,11 +4,18 @@ import dearpygui.dearpygui as dpg
 def create_gui():
     dpg.create_context()
 
-    with dpg.window(label="Casino Game"):
-        dpg.add_text("Hello!")
+    width, height, channels, data = dpg.load_image("images/cards.png")
 
-    dpg.create_viewport(title="Casino Game", width=400, height=300)
+    with dpg.texture_registry(show=False):
+        dpg.add_static_texture(width=width, height=height, default_value=data, tag="cards_tag")
+
+    with dpg.window(label="Casino", tag="casino_window"):
+        dpg.add_image("cards_tag", width=100, height=100)
+
+    dpg.set_primary_window("casino_window", True)
+    dpg.create_viewport(title='Custom Title', width=800, height=700, resizable=False)
     dpg.setup_dearpygui()
     dpg.show_viewport()
+    dpg.maximize_viewport()
     dpg.start_dearpygui()
-
+    dpg.destroy_context()
