80221bddf48df632312c2001f1c6436c23f6e28c
Jakub-Gulak <jakubgulak@icloud.com>
2025-02-13 12:36:56 +0100

    Reorganize project

---
+++ gui/dpg.py
@@ -0,0 +1,52 @@
+import dearpygui.dearpygui as dpg
+
+
+def update_position():
+
+    margin = 50
+    img_size = 190
+
+    width = dpg.get_viewport_width()
+    height = dpg.get_viewport_height()
+
+    dpg.configure_item("card_1", pos=(margin, margin))
+    dpg.configure_item("chip_1", pos=(margin, margin + img_size + margin))
+    dpg.configure_item("machine_1", pos=(margin, margin + 2 * (img_size + margin)))
+
+    dpg.configure_item("card_2", pos=(width - img_size - margin, margin))
+    dpg.configure_item("chip_2", pos=(width - img_size - margin, margin + img_size + margin))
+    dpg.configure_item("machine_2", pos=(width - img_size - margin, margin + 2 * (img_size + margin)))
+
+
+
+
+def create_gui():
+    dpg.create_context()
+
+    cards_width, cards_height, cards_channels, cards_data = dpg.load_image("images/cards.png")
+    chips_width, chips_height, chips_channels, chips_data = dpg.load_image("images/casino_chips.png")
+    machines_width, machines_height, machines_channels, machines_data = dpg.load_image("images/slot_machine.png")
+
+    with dpg.texture_registry(show=False):
+        dpg.add_static_texture(width=cards_width, height=cards_height, default_value=cards_data, tag="cards_tag")
+        dpg.add_static_texture(width=chips_width, height=chips_height, default_value=chips_data, tag="chips_tag")
+        dpg.add_static_texture(width=machines_width, height=machines_height, default_value=machines_data, tag="machines_tag")
+
+    with dpg.window(label="Casino", tag="casino_window"):
+        dpg.add_image("cards_tag", width=190, height=190, tag="card_1")
+        dpg.add_image("cards_tag", width=190, height=190, tag="card_2")
+        dpg.add_image("chips_tag", width=190, height=190, tag="chip_1")
+        dpg.add_image("chips_tag", width=190, height=190, tag="chip_2")
+        dpg.add_image("machines_tag", width=190, height=190, tag="machine_1")
+        dpg.add_image("machines_tag", width=190, height=190, tag="machine_2")
+
+    dpg.set_viewport_resize_callback(update_position)
+
+    dpg.set_primary_window("casino_window", True)
+    dpg.create_viewport(title='Custom Title', width=1400, height=900, resizable=True)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    update_position()
+    dpg.maximize_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
---
+++ main.py
@@ -0,0 +1,4 @@
+from gui.dpg import *
+
+if __name__ == "__main__":
+    create_gui()
