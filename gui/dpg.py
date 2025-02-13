eb2b398a6f76ccbb58eef395f6294faece85fb9d
Jakub-Gulak <jakubgulak@icloud.com>
2025-02-13 13:04:49 +0100

    (dpg.py) - add casino_button and input_name to mainpage, correct height of images

--- gui/dpg.py
+++ gui/dpg.py
@@ -2,22 +2,28 @@ import dearpygui.dearpygui as dpg
 
 
 def update_position():
+    width = dpg.get_viewport_width()
+    height = dpg.get_viewport_height()
 
     margin = 50
     img_size = 190
 
-    width = dpg.get_viewport_width()
-    height = dpg.get_viewport_height()
+    x = (width // 2) - 150
+    y = (height // 2) - 25
 
-    dpg.configure_item("card_1", pos=(margin, margin))
-    dpg.configure_item("chip_1", pos=(margin, margin + img_size + margin))
-    dpg.configure_item("machine_1", pos=(margin, margin + 2 * (img_size + margin)))
+    dpg.configure_item("card_1", pos=(margin, 100))
+    dpg.configure_item("chip_1", pos=(margin, 400))
+    dpg.configure_item("machine_1", pos=(margin, 700))
 
-    dpg.configure_item("card_2", pos=(width - img_size - margin, margin))
-    dpg.configure_item("chip_2", pos=(width - img_size - margin, margin + img_size + margin))
-    dpg.configure_item("machine_2", pos=(width - img_size - margin, margin + 2 * (img_size + margin)))
+    dpg.configure_item("card_2", pos=(width - img_size - margin, 100))
+    dpg.configure_item("chip_2", pos=(width - img_size - margin, 400))
+    dpg.configure_item("machine_2", pos=(width - img_size - margin, 700))
 
+    dpg.configure_item("input_name", pos=(x, y))
+    dpg.configure_item("casino_button", pos=(x, y + 60))
 
+def casino_button_click():
+    print("siema")
 
 
 def create_gui():
@@ -33,6 +39,9 @@ def create_gui():
         dpg.add_static_texture(width=machines_width, height=machines_height, default_value=machines_data, tag="machines_tag")
 
     with dpg.window(label="Casino", tag="casino_window"):
+        dpg.add_input_text( width=300, height=50, tag="input_name")
+        dpg.add_button(label="Go to Casino!", width=300, height=50,  tag="casino_button",  callback=casino_button_click)
+
         dpg.add_image("cards_tag", width=190, height=190, tag="card_1")
         dpg.add_image("cards_tag", width=190, height=190, tag="card_2")
         dpg.add_image("chips_tag", width=190, height=190, tag="chip_1")
