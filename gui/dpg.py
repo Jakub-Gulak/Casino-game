6926ef9796d4cbf239a5933126a86748f6ade5ea
Jakub-Gulak <jakubgulak@icloud.com>
2025-02-17 16:13:15 +0100

    (dpg.py) - add fonts directory, name_text and set new font

--- gui/dpg.py
+++ gui/dpg.py
@@ -19,8 +19,10 @@ def update_position():
     dpg.configure_item("chip_2", pos=(width - img_size - margin, 400))
     dpg.configure_item("machine_2", pos=(width - img_size - margin, 700))
 
+    dpg.configure_item("enter-name_text", pos=(x + 70, y - 60))
     dpg.configure_item("input_name", pos=(x, y))
     dpg.configure_item("casino_button", pos=(x, y + 60))
+
 
 def casino_button_click():
     print("siema")
@@ -33,14 +35,20 @@ def create_gui():
     chips_width, chips_height, chips_channels, chips_data = dpg.load_image("images/casino_chips.png")
     machines_width, machines_height, machines_channels, machines_data = dpg.load_image("images/slot_machine.png")
 
+    with dpg.font_registry():
+        font = dpg.add_font("fonts/Atkinson_Hyperlegible_Next/AtkinsonHyperlegibleNext-VariableFont_wght.ttf", 40)
+        dpg.bind_font(font)
+
     with dpg.texture_registry(show=False):
         dpg.add_static_texture(width=cards_width, height=cards_height, default_value=cards_data, tag="cards_tag")
         dpg.add_static_texture(width=chips_width, height=chips_height, default_value=chips_data, tag="chips_tag")
-        dpg.add_static_texture(width=machines_width, height=machines_height, default_value=machines_data, tag="machines_tag")
+        dpg.add_static_texture(width=machines_width, height=machines_height, default_value=machines_data,
+                               tag="machines_tag")
 
     with dpg.window(label="Casino", tag="casino_window"):
-        dpg.add_input_text( width=300, height=50, tag="input_name")
-        dpg.add_button(label="Go to Casino!", width=300, height=50,  tag="casino_button",  callback=casino_button_click)
+        dpg.add_text("Your name:", tag="enter-name_text")
+        dpg.add_input_text(width=300, height=50, tag="input_name")
+        dpg.add_button(label="Go to Casino!", width=300, height=50, tag="casino_button", callback=casino_button_click)
 
         dpg.add_image("cards_tag", width=190, height=190, tag="card_1")
         dpg.add_image("cards_tag", width=190, height=190, tag="card_2")
