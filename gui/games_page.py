41953beb155525dbfc6937c74ea534217b339c58
Jakub-Gulak <jakubgulak@icloud.com>
2025-03-03 09:43:00 +0100

    (dpg.py) - center greeting text

--- gui/games_page.py
+++ gui/games_page.py
@@ -9,7 +9,10 @@ def update_gamepage_position():
     width = dpg.get_viewport_width()
     height = dpg.get_viewport_height()
 
-    dpg.configure_item("name_text", pos=((width - 200) // 2, 20))
+    greeting_text = dpg.get_value("name_text")
+    greeting_width = dpg.get_text_size(greeting_text)[0]
+
+    dpg.configure_item("name_text", pos=((width - greeting_width) // 2, height // 6))
     dpg.configure_item("roulette_button", pos=((width - 300) // 2, (height // 2) - 50))
     dpg.configure_item("slot_button", pos=((width - 300) // 2, (height // 2)))
     dpg.configure_item("blackjack_button", pos=((width - 300) // 2, (height // 2) + 50))
@@ -32,12 +35,13 @@ def blackjack_button_click():
 
 def show_games_page():
     name = dpg.get_value("input_name")
+    greeting_text = f"Hello, {name}."
 
     width = dpg.get_viewport_width()
     height = dpg.get_viewport_height()
 
     with dpg.window(tag="casino_window2", pos=(0, 0), width=width, height=height, no_title_bar=True, no_move=True):
-        dpg.add_text(f"Hello, {name}.", tag='name_text')
+        dpg.add_text(greeting_text, tag="name_text")
         dpg.add_button(label="Roulette", width=300, tag="roulette_button", callback=roulette_button_click)
         dpg.add_button(label="Slot Machine", width=300, tag="slot_button", callback=machine_button_click)
         dpg.add_button(label="BlackJack", width=300, tag="blackjack_button", callback=blackjack_button_click)
