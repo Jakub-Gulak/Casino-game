60db3e63404a95fc662b484ef7322fe5fe4f7072
Jakub-Gulak <jakubgulak@icloud.com>
2025-03-24 09:51:33 +0100

    (dpg.py) - add buttons and their colors to roulette_colors.py

--- gui/roulette_colors.py
+++ gui/roulette_colors.py
@@ -9,9 +9,9 @@ def update_gamepage_position():
     dpg.configure_item("roulette_colors_text", pos=((width - text_width) // 2, height // 6))
     dpg.configure_item("roulette_colors_back_button", pos=((width - 300) // 2, (height // 2) + 300))
 
-    dpg.configure_item("roulette_red_button", pos=((width - 300) // 2, (height // 2) - 50))
+    dpg.configure_item("roulette_red_button", pos=((width - 1000) // 2, (height // 2)))
     dpg.configure_item("roulette_black_button", pos=((width - 300) // 2, (height // 2)))
-    dpg.configure_item("roulette_green_button", pos=((width - 300) // 2, (height // 2) + 50))
+    dpg.configure_item("roulette_green_button", pos=((width + 400) // 2, (height // 2)))
 
 
 def back_button_click():
@@ -31,19 +31,42 @@ def green_button_click():
     pass
 
 
+def create_themes():
+    with dpg.theme(tag="red_button_theme"):
+        with dpg.theme_component(dpg.mvButton):
+            dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [250, 0, 0])
+            dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [250, 0, 0])
+
+    with dpg.theme(tag="black_button_theme"):
+        with dpg.theme_component(dpg.mvButton):
+            dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [5, 5, 5])
+            dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [5, 5, 5])
+
+    with dpg.theme(tag="green_button_theme"):
+        with dpg.theme_component(dpg.mvButton):
+            dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [0, 200, 0])
+            dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [0, 200, 0])
+
+
 def roulette_colors_page():
     width = dpg.get_viewport_width()
     height = dpg.get_viewport_height()
 
-    with dpg.window(tag="roulette_colors_window", pos=(0, 0), width=width, height=height, no_title_bar=True, no_move=True):
+    create_themes()
+
+    with dpg.window(tag="roulette_colors_window", pos=(0, 0), width=width, height=height, no_title_bar=True,
+                    no_move=True):
         dpg.add_text(f"Roulette Colors", tag='roulette_colors_text')
 
-        dpg.add_button(label="Red", width=300, tag="roulette_red_button", callback=red_button_click)
-
-        dpg.add_button(label="Black", width=300, tag="roulette_black_button", callback=black_button_click)
-        dpg.add_button(label="Green", width=300, tag="roulette_green_button", callback=green_button_click)
+        red_button = dpg.add_button(label="Red", width=300, tag="roulette_red_button", callback=red_button_click)
+        black_button = dpg.add_button(label="Black", width=300, tag="roulette_black_button", callback=black_button_click)
+        green_button = dpg.add_button(label="Green", width=300, tag="roulette_green_button", callback=green_button_click)
 
         dpg.add_button(label="Back", width=300, tag="roulette_colors_back_button", callback=back_button_click)
+
+        dpg.bind_item_theme(red_button, "red_button_theme")
+        dpg.bind_item_theme(black_button, "black_button_theme")
+        dpg.bind_item_theme(green_button, "green_button_theme")
 
     dpg.set_primary_window("roulette_colors_window", True)
 
